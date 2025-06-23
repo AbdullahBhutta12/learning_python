53392ecf09bc21489e4b3178bf6163c138252209
AbdullahBhutta12 <116abdullahbhutta@gmail.com>
2025-06-23 16:50:27 +0500

    Test commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Pycharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Starting Python
+
+
+# String
+first_name = "Abdullah"
+food = "Pizza"
+email = "abdullah@gmail.com"
+print(f"Hello{first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
+print(len(email) )
+print(email[10])
+# Integers
+age = 20
+height = 5.6
+print(f"You are {age} years old")
+print(f"Your height is {height}")
