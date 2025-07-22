2d1cfa3427e5d481308d75f6b3817ce676930134
AbdullahBhutta12 <116abdullahbhutta@gmail.com>
2025-07-22 19:12:36 +0500

    Condition (if name.isalpha(): ) added to get just string input in names

--- second_task.py
+++ second_task.py
@@ -10,13 +10,16 @@ if num_elements.isdigit():
     for i in range(num_elements):
         while True:
             name = input(f"Enter person {i + 1} name: ")
-            name = name.lower()
-            if name in persons:
-                print("This name is already taken.")
+            if name.isalpha():
+                name = name.lower()
+                if name in persons:
+                    print("This name is already taken.")
+                else:
+                    persons[name] = 0
+                    break
 
             else:
-                persons[name] = i + 1
-                break
+                print("Warning: Only give string input.")
 
     print(f"\nYou are {num_elements} persons {persons}")
 
@@ -51,18 +54,20 @@ if num_elements.isdigit():
             else:
                 os.system('clear')
 
-
     print("\nResult:")
     for name, point in persons.items():
         print(f"Total points of {name} are {point}")
-
 
     print("\nAverage:")
     for name, point in persons.items():
         average = point / num_elements
         print(f"Average points of {name} are {average:.2f}")
+    #
+    # file_path = "/home/abdullah-saeed/results_2nd-task.txt"
+    # txt = "Results: \n"
+    # with open(file_path, "w") as file:
+    #     file.write(txt)
 
     print("\nThanks")
 else:
     print("Invalid input: Plz enter integer value only")
-
